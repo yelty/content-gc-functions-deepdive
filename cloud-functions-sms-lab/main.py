bcd88776b6434518356e7637bc4ea5e048797f9e
Christophe Cosnefroy <christophe.cosnefroy@yelty.fr>
2019-10-25 14:13:05 +0200

    Update main.py

--- cloud-functions-sms-lab/main.py
+++ cloud-functions-sms-lab/main.py
@@ -4,21 +4,21 @@ import twilio
 
 from twilio.rest import Client
 
-TWILIO_ACCOUNT_SID = "[YOUR_TWILIO_ACCOUNT_SID]"
-TWILIO_AUTH_TOKEN = "[YOUR_TWILIO_AUTH_TOKEN]"
-TWILIO_NUMBER = "[YOUR_TWILIO_NUMBER]"
-TO_NUMBERS = ['ARRAY_OF_NUMBERS']
-BUCKET = "[BUCKET_NAME]"
+TWILIO_ACCOUNT_SID = getenv('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = getenv('TWILIO_AUTH_TOKEN')
+TWILIO_NUMBER = getenv('TWILIO_NUMBER')
+
+BUCKET = getenv('BUCKET', 'sms-input')
 
 def send_sms(data,context):
 
     theBody = "Item added to Cloud Storage bucket '" + BUCKET + "'.\n" + "Item added: " + data['name']
 
     client = twilio.rest.Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    for TO_NUMBER in TO_NUMBERS:
-        rv = client.messages.create(
-            to= TO_NUMBER,
-            from_= TWILIO_NUMBER,
-            body= theBody
-        )
+
+    rv = client.messages.create(
+         to= TWILIO_NUMBER,
+         from_= TWILIO_NUMBER,
+         body= theBody
+     )
     return str(rv)
