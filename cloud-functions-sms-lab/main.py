dadb37460b6b944d04b33ce75593f3f653dd1260
Christophe Cosnefroy <christophe.cosnefroy@yelty.fr>
2019-10-25 14:22:40 +0200

    Update main.py

--- cloud-functions-sms-lab/main.py
+++ cloud-functions-sms-lab/main.py
@@ -8,7 +8,7 @@ from twilio.rest import Client
 TWILIO_ACCOUNT_SID = getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = getenv('TWILIO_AUTH_TOKEN')
 TWILIO_NUMBER = getenv('TWILIO_NUMBER')
-
+DEST_NUMBER = getenv('DEST_NUMBER')
 BUCKET = getenv('BUCKET', 'sms-input')
 
 def send_sms(data,context):
@@ -18,7 +18,7 @@ def send_sms(data,context):
     client = twilio.rest.Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
     rv = client.messages.create(
-         to= TWILIO_NUMBER,
+         to= DEST_NUMBER,
          from_= TWILIO_NUMBER,
          body= theBody
      )
